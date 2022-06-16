f124447122da311391fc5e8588225535306b920d
Gian Manzo <ggianmanzo@gmail.com>
2022-06-16 17:20:03 -0300

    accordion transform class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 import ScrollSmooth from './modules/scroll-suave.js'
+import Accordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
@@ -6,13 +7,15 @@ import initDropDown from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import initFetchAnimais from './modules/fetch-animais.js'
-import initAccordion from './modules/accordion.js'
-import initFetchBitcoin from './modules/fetch-bitcoin.js'
 import initScrollReveal from './modules/scroll-reveal.js'
+import initFetchBitcoin from './modules/fetch-bitcoin.js'
 
 const scrollSmooth = new ScrollSmooth('[data-js="menu"] a[href^="#"]')
 scrollSmooth.init()
-initAccordion()
+
+const accordion = new Accordion('[data-anima="accordion"] dt')
+accordion.init()
+
 initTabNav()
 initModal()
 initTooltip()
